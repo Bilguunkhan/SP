de7c858c1adf45e29af2025289248c1de42dc7da
BillyGoon <bilguun_iqo@yahoo.com>
2018-03-14 15:12:19 +0200

    trained model 1

---
+++ test.py
@@ -0,0 +1,4 @@
+import predict as pred
+
+#print(pred.classifySkinLesion("C:/Users/Bilguun.NOMLOG/Desktop/SP/test/cancer.jpg"))
+print(pred.classifySkinLesion("C:/Users/Bilguun.NOMLOG/Desktop/SP/test/benign.jpg"))
